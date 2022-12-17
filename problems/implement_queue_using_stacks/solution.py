2b24de031e03f9dff09f3e4086230e3e67396713
洪健翔 Hung, Chien-Hsiang <abcdefg2981@gmail.com>
2022-12-17 02:39:40 +0000

    Sync LeetCode submission - Implement Queue using Stacks (python3)

--- problems/implement_queue_using_stacks/solution.py
+++ problems/implement_queue_using_stacks/solution.py
@@ -1,32 +1,27 @@
-class QNode:
-    def __init__(self, v=0, _next=None):
-        self.value = v
-        self.next = _next
-
-
 class MyQueue:
+
     def __init__(self):
-        self.root = None
-        self.tail = None
+        self.in_stack = []
+        self.out_stack = []
 
     def push(self, x: int) -> None:
-        if not self.root:
-            self.root = QNode(x)
-            self.tail = self.root
-        else:
-            self.tail.next = QNode(x)
-            self.tail = self.tail.next
-        
+        self.in_stack.append(x)
+
     def pop(self) -> int:
-        output = self.root.value
-        self.root = self.root.next
-        return output
-
+        if not self.out_stack:
+            while self.in_stack:
+                self.out_stack.append(self.in_stack.pop())
+        return self.out_stack.pop()
+        
     def peek(self) -> int:
-        return self.root.value
-
+        if not self.out_stack:
+            return self.in_stack[0]
+        else:
+            return self.out_stack[-1]
+        
     def empty(self) -> bool:
-        return not self.root
+        return not self.in_stack and not self.out_stack
+        
 
 
 # Your MyQueue object will be instantiated and called as such:
