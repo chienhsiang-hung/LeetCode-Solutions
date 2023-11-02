9ce8b03b18144cb0cdc90f9fdd31c314c79d2125
洪健翔 Hung, Chien-Hsiang <abcdefg2981@gmail.com>
2023-11-02 05:47:28 +0000

    Sync LeetCode submission - Two Furthest Houses With Different Colors (python3)

--- problems/two_furthest_houses_with_different_colors/solution.py
+++ problems/two_furthest_houses_with_different_colors/solution.py
@@ -1,13 +1,15 @@
 class Solution:
     def maxDistance(self, colors: List[int]) -> int:
-        # O(n^2)
-        global_max = 0
-        for i in range(len(colors)-1):
-            local_max = len(colors)-i-1
-            for j in range(len(colors)-1, i, -1):
-                if colors[i] != colors[j]:
-                    if local_max > global_max:
-                        global_max = local_max
-                        break
-                local_max -= 1
-        return global_max
+        # O(n)
+        if colors[0] != colors[len(colors)-1]:
+            return len(colors)-1
+        
+        my_max = 0
+        for i in range(1, len(colors)):
+            if colors[i] != colors[0]:
+                my_max = max(
+                    my_max, max(i-0, len(colors)-i-1)
+                )                
+
+        return my_max
+            
