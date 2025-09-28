ad8a39905399638031e4b4bdb47e863dca274df1
洪健翔 Hung, Chien-Hsiang <abcdefg2981@gmail.com>
2025-09-28 07:03:46 +0000

    Sync LeetCode submission Runtime - 186 ms (21.54%), Memory - 18 MB (87.08%)

--- 0424-longest-repeating-character-replacement/solution.py
+++ 0424-longest-repeating-character-replacement/solution.py
@@ -3,7 +3,6 @@ class Solution:
         c_map = dict()
         
         ans = 0
-        max_count_c = 0
         l = 0
         for r in range(len(s)):
             if s[r] not in c_map or not c_map[s[r]]:
@@ -11,10 +10,8 @@ class Solution:
             else:
                 c_map[s[r]] += 1
 
-            max_count_c = max(max_count_c, c_map[s[r]])
-            while r+1-l - max_count_c > k:
+            while r+1-l - max(c_map.values()) > k:
                 c_map[s[l]] -= 1
-                max_count_c = max(max_count_c, c_map[s[l]])
                 l += 1
             ans = max(ans, r+1-l)
         
