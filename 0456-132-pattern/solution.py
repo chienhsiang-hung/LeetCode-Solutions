7081018729dd2b2015a355a2881a688be61dd358
洪健翔 Hung, Chien-Hsiang <abcdefg2981@gmail.com>
2025-10-15 07:47:56 +0000

    Sync LeetCode submission Runtime - 55 ms (57.59%), Memory - 35.3 MB (56.31%)

---
+++ 0456-132-pattern/solution.py
@@ -0,0 +1,26 @@
+'''
+[3,5,0,1,2,4]
+[4]
+[4,2]
+[4,2,1]
+[4,2,1,0]
+
+[3,5,0,3,4,1]
+5: [1]
+4: k=1, [4]
+3: k=1, [4,3]
+'''
+class Solution:
+    def find132pattern(self, nums: List[int]) -> bool:
+        stack = []
+        k = float('-inf')
+
+        for i in range(len(nums)-1, -1, -1):
+            if nums[i] < k:
+                return True
+            while stack and nums[i] > stack[-1]:
+                k = stack.pop()
+            stack.append(nums[i])
+        return False
+        
+
