d814c91feb5703de4869fc86be322fcc1f0c1e6f
洪健翔 Hung, Chien-Hsiang <abcdefg2981@gmail.com>
2025-10-16 09:50:33 +0000

    Sync LeetCode submission Runtime - 11 ms (8.59%), Memory - 20.9 MB (5.98%)

--- 0210-course-schedule-ii/solution.py
+++ 0210-course-schedule-ii/solution.py
@@ -1,49 +1,55 @@
-from collections import defaultdict
+'''
+[[1,0],[2,0],[3,1],[3,2]]
+0: N
+1: 0
+2: 0
+3: 1,2
 
+1>0>N [0,1]
+0: N
+1: N
+2: 0
+3: 1,2
+
+2>0>N [0,1,2]
+0: N
+1: N
+2: N
+3: 1,2
+
+[[1,0]]
+'''
+from collections import defaultdict
 class Solution:
     def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
-        '''
-        [[1,0], [2,0], [3,1], [3,2]]
-
-        1:0
-        0:1
-
-        1:0,2
-        2:0
-
-        [[1,0]]
-        '''
-        courses = defaultdict(list)
+        csMap = defaultdict(set)
         for c, p in prerequisites:
-            courses[c].append(p)
+            csMap[c].add(p)
         
-        ans = []
-        ans_added = set()
-
-        def findPre(c):
+        def dfs(c):
             if c in vis:
                 return False
             vis.add(c)
-            if not courses[c] and c not in ans_added:
-                ans.append(c)
-                ans_added.add(c)
-                return True
-            while courses[c]:
-                curr_c = courses[c].pop()
-                if not findPre(curr_c): return False
-                vis.remove(curr_c)
-            if c not in ans_added:
-                ans.append(c)
-                ans_added.add(c)
-            return True
-                
 
+            if csMap[c]:
+                for p in set(csMap[c]):
+                    
+                    if not dfs(p): return False
+                    csMap[c].remove(p)
+                    vis.remove(p)
+                if c not in taken:
+                    taken.add(c)
+                    res.append(c)
+                return True
+
+            if c not in taken:
+                taken.add(c)
+                res.append(c)
+            return True
+        
+        res = []
+        taken = set()
         for c in range(numCourses):
-            if not courses[c] and c not in ans_added:
-                ans.append(c)
-                ans_added.add(c)
-            else:
-                vis = set()
-                if not findPre(c):
-                    return []
-        return ans
+            vis = set()
+            if not dfs(c): return []
+        return res if len(res) == numCourses else []
