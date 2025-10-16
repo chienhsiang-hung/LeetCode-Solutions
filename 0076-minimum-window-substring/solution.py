2ee638ee9f23c0412d98db20f344fba5980f4cec
洪健翔 Hung, Chien-Hsiang <abcdefg2981@gmail.com>
2025-10-16 10:34:27 +0000

    Sync LeetCode submission Runtime - 127 ms (33.70%), Memory - 18.3 MB (53.22%)

--- 0076-minimum-window-substring/solution.py
+++ 0076-minimum-window-substring/solution.py
@@ -1,27 +1,40 @@
-from collections import defaultdict
+'''
+s = ""ADOBECODEBANC", t = "ABC"
+ADOBEC
+DOBECODEBA
+'''
 class Solution:
     def minWindow(self, s: str, t: str) -> str:
-        if len(s) < len(t): return ''
-
-        t_dict = defaultdict(int)
-        for char in t:
-            t_dict[char] += 1
-        missing = len(t)
-
-        l = best_l = 0
-        best_r = float('inf')
-        for i, char in enumerate(s):
-            t_dict[char] -= 1
-            if t_dict[char] >= 0:
-                missing -= 1
+        countC = dict()
+        for c in t:
+            if c not in countC:
+                countC[c] = 1
+            else:
+                countC[c] += 1
+        needed = len(t)
+        
+        l = 0
+        window_size = float('inf')
+        ans = ''
+        for i, c in enumerate(s):
+            if c in countC:
+                countC[c] -= 1
+                if countC[c] >= 0:
+                    needed -= 1
             
-            while missing == 0:
-                t_dict[s[l]] += 1
-                if t_dict[s[l]] > 0:
-                    missing += 1
-                    if i-l < best_r-best_l:
-                        best_l = l
-                        best_r = i
+            if needed == 0 and i-l+1 < window_size:
+                ans = s[l:i+1]
+                window_size = i+1-l
+            
+            while needed == 0:
+                if i-l+1 < window_size:
+                    ans = s[l:i+1]
+                    window_size = i+1-l
+
+                if s[l] in countC:
+                    countC[s[l]] += 1
+                    if countC[s[l]] > 0:
+                        needed += 1
                 l += 1
-        return s[best_l:best_r+1] if best_r != float('inf') else ''
-                    
+        
+        return ans
